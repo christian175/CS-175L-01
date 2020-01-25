1f7acf1a6b9cdc6d13a8211d1c97242aa62412aa
christian175 <55259741+christian175@users.noreply.github.com>
2020-01-25 15:53:08 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,12 +2,8 @@ import java.util.Scanner;
 public class hello {
 
     public static void main(String[] args) {
-      Scanner myObj = new Scanner(System.in);
-      System.out.println("What is your name? ");
 
-      String Name = myObj.nextLine();
-      System.out.println("Hello " + Name + "!");
-      
+      System.out.println("Hello World!");
     }
 
 }
