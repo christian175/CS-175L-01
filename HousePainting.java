c2c5aefa2322e4d900a2bd00de390d2cf54f71f9
christian175 <55259741+christian175@users.noreply.github.com>
2020-02-03 12:24:56 -0500

    t

--- HousePainting.java
+++ HousePainting.java
@@ -3,28 +3,30 @@ public class HousePainting {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.print("Input house length:");
+		//prompting the user to input values and storing them
+		System.out.print("Input house length: ");
 		int houseLength = input.nextInt();
-		System.out.print("Input house height:");
+		System.out.print("Input house height: ");
 		int houseHeight = input.nextInt();
-		System.out.println("Input house width:");
+		System.out.println("Input house width: ");
 		int houseWidth = input.nextInt();
-		System.out.print("Input amount of windows:");
+		System.out.print("Input amount of windows: ");
 		int windowNum = input.nextInt();
-		System.out.print("Input widow height:");
+		System.out.print("Input widow height: ");
 		int windowHeight = input.nextInt();
-		System.out.print("Input window length:");
+		System.out.print("Input window length: ");
 		int windowLength = input.nextInt();
-		System.out.print("Input amount of doors:");
+		System.out.print("Input amount of doors: ");
 		int doorNum = input.nextInt();
-		System.out.print("Input door height:");
+		System.out.print("Input door height: ");
 		int doorHeight = input.nextInt();
-		System.out.print("Input door length:");
+		System.out.print("Input door length: ");
 		int doorLength = input.nextInt();
-		System.out.print("Input painter rate per square unit:");
+		System.out.print("Input painter rate per square unit: ");
 		int painterCost = input.nextInt();
+		//performing the necessary calculations to find the price
 		int noPeakSides =  houseLength *  houseWidth * 2;
-		int peakSides = noPeakSides + ((1/2)(houseLength*(houseHeight -  houseWidth)));
+		int peakSides = noPeakSides + ((1/2)*(houseLength*(houseHeight -  houseWidth)));
 		peakSides = peakSides * 2;
 		int windowSize =  windowNum * windowHeight * windowLength;
 		int doorSize =  doorNum * doorLength * doorHeight;
@@ -32,6 +34,7 @@ public class HousePainting {
 		int noPaint = windowSize + doorSize;
 		int paintArea = sidesSize - noPaint;
 		int totalCost =  paintArea * painterCost;
+		//outputting the price
 		System.out.println("Your total is: " + totalCost);
 
 
