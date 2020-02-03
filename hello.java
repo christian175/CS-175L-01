244734807539d404082e6d63687138b45e2850b6
christian175 <55259741+christian175@users.noreply.github.com>
2020-02-03 12:25:31 -0500

    Update hello.java

--- hello.java
+++ hello.java
@@ -2,8 +2,12 @@
 public class hello {
 
     public static void main(String[] args) {
-    	double grade = 99;
-    	grade = grade + grade;
-    	System.out.println(grade);
+    	int i = 12, j = 5, k =-3;
+    	double x = 2.1, y = -1.5, z = 3.0;
+    	System.out.println((i + j + k) / 3);
+    	System.out.println((i / j) * j + (i % j));
+    	System.out.println(x * x  + y * y);
+    	System.out.println((x + y + z) / (x - y -z));
+    	System.out.println(2.0 * z - (x + y));
     }
 }
