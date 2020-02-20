e4ba68d821811d20f28d26354b9c9353dd0f6c7d
christian175 <55259741+christian175@users.noreply.github.com>
2020-02-20 12:17:19 -0500

    groceryV1

--- grocerydiscount.java
+++ grocerydiscount.java
@@ -9,7 +9,14 @@ public class grocerydiscount {
 		double discount;
 		boolean on = true;
 		while (on) {
-			if (price < 10) {
+			if (price == 0) {
+				on = false;
+			}
+			else if (price < 0) {
+				System.out.println("Error");
+				on = false;
+			}
+			else if (price < 10) {
 				System.out.println("You are not eligible for a coupon at this time");
 			}
 			else if (price < 60) {
@@ -18,6 +25,14 @@ public class grocerydiscount {
 			}
 			else if (price < 150) {
 				discount = price * 0.1;
+				System.out.println("you are eligible for a discount of " + discount);
+			}
+			else if (price < 210) {
+				discount = price * 0.12;
+				System.out.println("you are eligible for a discount of " + discount);
+			}
+			else {
+				discount = price * 0.14;
 				System.out.println("you are eligible for a discount of " + discount);
 			}
 		}
