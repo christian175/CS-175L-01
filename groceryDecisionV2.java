3e7d1aef3e20aae56f729c32a8de2a1532b7d1e9
christian175 <55259741+christian175@users.noreply.github.com>
2020-02-24 20:56:35 -0500

    groceryV2

--- groceryDecisionV2.java
+++ groceryDecisionV2.java
@@ -11,39 +11,38 @@ public class grocerydecisionV2 {
 			try {
 				price = Double.parseDouble(in);
 			} catch (NumberFormatException ex) {
-				System.out.println("That is not a valid input.");
+				System.out.println("Error, that is not a valid input.");
 			}
 			double discount;
 			if (price == 0) {
 				on = false;
 			}
-			else if (price >= 10 && price <= 1000) {
+			else if (price < 1000) {
 				if (price < 0) {
-					System.out.println("Error, too low.");
-					on = false;
+					System.out.println("Error, that is not a valid number.");
 				}
 				else if (price < 10) {
 					System.out.println("You are not eligible for a coupon at this time.");
 				}
 				else if (price < 60) {
 					discount = price * 0.08;
-					System.out.println("you win a discount of " + discount);
+					System.out.printf("you win a discount of %6.2f%n", discount);
 				}
 				else if (price < 150) {
 					discount = price * 0.1;
-					System.out.println("you win a discount of " + discount);
+					System.out.printf("you win a discount of %6.2f%n", discount);
 				}
 				else if (price < 210) {
 					discount = price * 0.12;
-					System.out.println("you win a discount of " + discount);
-				}
-				else if (price < 1000) {
-					discount = price * 0.14;
-					System.out.println("you win a discount of " + discount);
+					System.out.printf("you win a discount of %6.2f%n", discount);
 				}
 				else {
-					System.out.println("Too high.");
+					discount = price * 0.14;
+					System.out.printf("you win a discount of %6.2f%n", discount);
 				}
+			}
+			else {
+				System.out.println("Error, too high.");
 			}
 		}
 
