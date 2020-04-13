22ab4bc832c0e78c6e0dd5edc3a321de158e0ca0
christian175 <55259741+christian175@users.noreply.github.com>
2020-04-13 12:39:39 -0400

    f

---
+++ Roaches/roachPopulation.java
@@ -0,0 +1,20 @@
+
+public class roachPopulation {
+	public int roachNum;
+	
+	public roachPopulation(int x) {
+		roachNum = x;
+	}
+	
+	public void breed() {
+		roachNum = roachNum * 2;
+	}
+	
+	public void spray(double y) {
+		roachNum = (int) (Math.round(roachNum - (roachNum * (y / 100))));
+	}
+	
+	public int getRoaches() {
+		return roachNum;
+	}
+}
